255bfd52f638744364c0fb1e3264531f4185269c
Bruno Ramos Lemos <bruno.lemos@live.com>
2023-01-14 15:17:32 -0300

    Ola2

--- src/server.js
+++ src/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const routes = require('./routes')
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
-
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message})
+})
 
 
 
