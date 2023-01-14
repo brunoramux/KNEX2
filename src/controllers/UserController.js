1c7910dddca747a6c0c7b893b1e05eba47b1c8ff
Bruno Ramos Lemos <bruno.lemos@live.com>
2023-01-14 16:21:14 -0300

    Finalizando CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,5 +16,30 @@ module.exports = {
             next(error)
         }
        
+    },
+    async update(req, res, next){
+        try {
+            const { username } = req.body
+            const { id } = req.params
+
+            await knex('users').update({ username }).where({ id })
+
+            return res.send()
+        } catch (error){
+            next(error)
+        }
+    },
+
+    async delete(req, res, next){
+        try {
+            
+            const { id } = req.params
+
+            await knex('users').delete().where({ id })
+
+            return res.send()
+        } catch (error){
+            next(error)
+        }
     }
 }
